366a6eeb111437bf48de68534b6fd06e770a4c37
Orlando Lucero <orlando.lucero.camacho@gmail.com>
2022-05-16 01:40:24 -0500

    Login terminado

--- lib/src/screens/login_page.dart
+++ lib/src/screens/login_page.dart
@@ -26,12 +26,12 @@ class _LoginPageState extends State<LoginPage> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
-                margin: const EdgeInsets.all(60),
+                margin: const EdgeInsets.only(top: 50, bottom: 60, right: 120, left: 120),
                 color: const Color(0xFFBAD1FF),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
-                    horizontal: 35,
-                    vertical: 20,
+                    horizontal: 120,
+                    vertical: 80,
                   ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -65,7 +65,7 @@ class _LoginPageState extends State<LoginPage> {
                         ],
                       ),
                       const SizedBox(
-                        height: 150,
+                        height: 85,
                       ),
                       Row(
                         children: <Widget>[
@@ -77,7 +77,7 @@ class _LoginPageState extends State<LoginPage> {
                                   borderRadius: BorderRadius.circular(15),
                                   child: Image.asset(
                                     'assets/images/login_desktop.jpg',
-                                    height: 500,
+                                    height: 510,
                                   ),
                                 ),
                               ],
@@ -99,16 +99,19 @@ class _LoginPageState extends State<LoginPage> {
                                 ),
                                 const SizedBox(height: 20),
                                 Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  //crossAxisAlignment: CrossAxisAlignment.stretch,
+                                  //crossAxisAlignment: CrossAxisAlignment.stretch,
                                   children: [
                                     Expanded(
                                       child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.end,
                                         children: [
                                           Container(
                                             width: 220,
-                                            height: 50,
+                                            height: 40,
                                             alignment: Alignment.center,
-                                            padding: const EdgeInsets.all(10.0),
+                                            padding: const EdgeInsets.all(5.0),
                                             decoration: BoxDecoration(
                                               borderRadius: BorderRadius.circular(15.0),
                                               color: const Color(0xFF01325E),
@@ -124,13 +127,14 @@ class _LoginPageState extends State<LoginPage> {
                                         ],
                                       ),
                                     ),
-                                    const SizedBox(width: 30),
+                                    const SizedBox(width: 50),
                                     Expanded(
                                       child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
                                         children: <Widget>[
                                           Container(
-                                            width: 600,
-                                            height: 50,
+                                            width: 250,
+                                            height: 40,
                                             alignment: Alignment.center,
                                             padding: const EdgeInsets.all(10.0),
                                             decoration: BoxDecoration(
@@ -150,13 +154,13 @@ class _LoginPageState extends State<LoginPage> {
                                   children: [
                                     Expanded(
                                       child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.end,
                                         children: [
                                           Container(
                                             width: 220,
-                                            height: 50,
+                                            height: 40,
                                             alignment: Alignment.center,
-                                            padding: const EdgeInsets.all(10.0),
+                                            padding: const EdgeInsets.all(5.0),
                                             decoration: BoxDecoration(
                                               borderRadius: BorderRadius.circular(15.0),
                                               color: const Color(0xFF01325E),
@@ -172,13 +176,14 @@ class _LoginPageState extends State<LoginPage> {
                                         ],
                                       ),
                                     ),
-                                    const SizedBox(width: 30),
+                                    const SizedBox(width: 50),
                                     Expanded(
                                       child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
                                         children: <Widget>[
                                           Container(
-                                            width: 600,
-                                            height: 50,
+                                            width: 250,
+                                            height: 40,
                                             alignment: Alignment.center,
                                             padding: const EdgeInsets.all(10.0),
                                             decoration: BoxDecoration(
@@ -194,31 +199,29 @@ class _LoginPageState extends State<LoginPage> {
                                   ],
                                 ),
                                 const SizedBox(height: 50),
-                                ElevatedButton(
-                                    style: _elevatedButtonStyle(context),
-                                    onPressed: () => _login(context),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        const Text('Iniciar sesión'),
-                                        if (_loading)
-                                          Container(
-                                            height: 20,
-                                            width: 20,
-                                            margin: const EdgeInsets.only(left: 20),
-                                            child: const CircularProgressIndicator(
-                                              color: Colors.white,
-                                            ),
-                                          )
-                                      ],
-                                  ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          ElevatedButton (
+                                            style: _elevatedButtonStyle(context),
+                                            onPressed: () => _login(context),
+                                            child: const Text('Iniciar sesión'),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 150,),
                     ],
                   ),
                 ),
@@ -234,7 +237,7 @@ class _LoginPageState extends State<LoginPage> {
     return ElevatedButton.styleFrom(
       textStyle: const TextStyle(fontSize: 20),
       primary: Theme.of(context).primaryColor,
-      padding: const EdgeInsets.symmetric(vertical: 15),
+      padding: const EdgeInsets.only(top:15, bottom: 15, left: 30, right: 30),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
